f4f413f748265998513943f7e60b8a3620d4243a
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2021-10-31 00:32:56 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
-<html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-   <?php echo "Hello World!";?>
- </body>
-</html>
+<?php
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      
+    }else{
+      echo"Bem-Vindo, Estudante! <br>";
+      echo"<br><a href='login.html'>Faça Login</a>";
+    }
+?>
