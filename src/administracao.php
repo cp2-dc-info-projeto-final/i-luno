9db52bb03953de63f56e53c3ff10e3bb55139afb
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2021-11-30 00:41:31 -0300

    Update administracao.php

--- src/administracao.php
+++ src/administracao.php
@@ -1,5 +1,8 @@
 <html>
 <head>
     <h1> Area de administracao</h1>
+    <a href="edicaoUsu.php"> Editar usuario </a>
+    <a href="excluiUsu.php"> Exclui usuario </a>
+    <a href="atualizaUsu.php"> Atualizar usuario </a>
 </head>
 </html>
