e98655d3047b05ec593ccb59eb4ab77ca55ebbc0
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2021-12-13 23:20:22 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,66 +1,70 @@
-<?php
-  session_start();
-?> 
-<html>
 <head>
-<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="interface.css" media="screen" />
+  <meta charset="UTF-8" />
+  <title>I-luno</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-
-<h2>Faça seu login</h2>
-
-<form action="operacao.php" method="POST">
-  <div class="imgcontainer">
-    <img src="logo.png" class="logo">
-  </div>
-
-  <div class="container">
-    <form class="form-signin" method="POST" action="operacao.php">
-    <label for="uname"><b>Nome de Usuário</b></label>
-    <input type="text" placeholder="Digite seu nome de usuário" name="uname" required>
-
-    <label for="psw"><b>Senha</b></label>
-    <input type="password" placeholder="Digite sua senha" name="psw" required>
-        
-    <button type="submit" style="background-color:#43a759">Login</button>
-    
-    <button type="submit" style="background-color:#43a759">Cadastrar</button>
-  </div>
-    
-  
-
-
-</form>
-<p class="text-center text-danger">
-			<?php if(isset($_SESSION['loginErro'])){
-				echo $_SESSION['loginErro'];
-				unset($_SESSION['loginErro']);
-			}?>
-		</p>
-		<p class="text-center text-success">
-			<?php 
-			if(isset($_SESSION['logindeslogado'])){
-				echo $_SESSION['logindeslogado'];
-				unset($_SESSION['logindeslogado']);
-			}
-			?>
-		</p>
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-    <script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
+  <div class="container" >
+    <a class="links" id="paracadastro"></a>
+    <a class="links" id="paralogin"></a>
+     
+    <div class="content">      
+      <!--FORMULÁRIO DE LOGIN-->
+      <div id="login">
+        <form method="post" action="login.php"> 
+          <h1>Login</h1> 
+          <p> 
+            <label for="nomUsuario"> Seu Nome de usuário </label>
+            <input name="cadastro" required="required" type="text" placeholder="Digite seu nome de usuário" /> 
+           
+          <p> 
+            <label for="nomUsuario">Sua senha</label>
+            <input name="cadastro" required="required" type="password" placeholder="Diigte sua senha" /> 
+          </p>
+                 
+          <p> 
+            <input type="submit" value="Logar" /> 
+          </p>
+           
+          <p class="link">
+            Ainda não possui uma conta?
+            <a href="#paracadastro">Cadastre-se</a>
+          </p>
+        </form>
+      </div>
+ 
+      <!--FORMULÁRIO DE CADASTRO-->
+      <div id="cadastro">
+        <form method="post" action="cadastro.php"> 
+          <h1>Cadastro</h1> 
+           
+          <p> 
+            <label for="nome">Seu nome</label>
+            <input name="cadastro" required="required" type="text" placeholder="Entre com o seu nome (até 20 caracteres)" />
+          </p>
+           
+          <p> 
+            <label for="nomUsuario">Seu nome de usuário</label>
+            <input name="cadastro" required="required" type="text" placeholder="Crie um nome de usuário (entre 5 e 10 caracteres)"/> 
+          </p>
+           
+          <p> 
+            <label for="senha">Sua senha</label>
+            <input name="cadastro" required="required" type="password" placeholder="Crie sua senha (entre 5 e 10 caracteres)"/>
+          </p>
+           
+          <p> 
+            <input type="submit" value="Cadastrar"/> 
+          </p>
+           
+          <p class="link">  
+            Já tem conta?
+            <a href="#paralogin"> Ir para Login </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>  
 </body>
 </html>
