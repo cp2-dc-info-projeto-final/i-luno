afcf0cb724082d7819697b0f8e313d9bfee60bdc
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-02-24 11:05:56 -0300

    Delete administracao.php

--- src/administracao.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <h1> Area de administracao</h1>
-    <a href="edicaoUsu.php"> Editar usuario </a>
-    <a href="excluiUsu.php"> Exclui usuario </a>
-    <a href="atualizaUsu.php"> Atualizar usuario </a>
-</head>
-</html>
