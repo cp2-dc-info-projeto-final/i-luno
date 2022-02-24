c11ec7aec5bb2098f55f12b51052f3aa13455b4c
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-02-24 15:23:07 -0300

    Update admin.php

--- src/admin.php
+++ src/admin.php
@@ -28,23 +28,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>SISTEMA DE ADMINISTRAÇÃO </title>
-    <style>
-        body{
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-            color: white;
-            text-align: center;
-        }
-        .table-bg{
-            background: rgba(0, 0, 0, 0.3);
-            border-radius: 15px 15px 0 0;
-        }
-
-        .box-search{
-            display: flex;
-            justify-content: center;
-            gap: .1%;
-        }
-    </style>
+    
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
