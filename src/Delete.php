eb6a1f8ac4726695b35cac54edc1586d0d475c06
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-03-18 12:06:35 -0300

    Update and rename DeletarUsu.php to Delete.php

--- src/Delete.php
+++ src/Delete.php
@@ -2,20 +2,20 @@
 
     if(!empty($_GET['id']))
     {
-        include_once('conecta.php');
+      include_once('conecta.php');
 
         $id = $_GET['id'];
 
         $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
 
-        $result = $conexao->query($sqlSelect);
+        $result = $conexao ->query($sqlSelect);
 
         if($result->num_rows > 0)
         {
             $sqlDelete = " DELETE FROM usuarios WHERE id=$id";
-            $resultDelete = $conexao->query($sqlDelete);
+            $resultDelete = $conexao ->query($sqlDelete);
         }
     }
-    header('Location: formulario.php');
+    header('Location: admin.php');
    
 ?>
