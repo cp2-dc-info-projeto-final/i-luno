3ea18ce94859efd9c9dace1a7f86850c168898a9
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-18 17:22:42 -0300

    Update dentro.php

--- src/dentro.php
+++ src/dentro.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 
 session_start();
 //por um link pro adm ir direto pra lista,busca,exclusão,edição e atualização
@@ -7,16 +7,12 @@ session_start();
 
 if(isset($_SESSION['adm'])){ 
     echo'Bem-vindo'. $_SESSION ['adm'].' - ADMINISTRADOR';
-      header(location:admin.php);
-    
-    }else if (isset($_SESSION['normal'])){  
-        echo 'Bem-vindo'. $_SESSION['normal']. ''; 
-    header(location:paginainicial.php);
+    echo '<script type=" text/javascript">window.location = "admin.php"</script>';
+     }else if (isset($_SESSION['normal'])){  
+        echo 'Bem-vindo'. $_SESSION['normal']. '';
+        echo '<script type=" text/javascript">window.location = "paginainicial.php"</script>';
+        }else{  
       
-
-    }else{  
-      echo '<script type=" text/javascript">window.location = "formulario.php"</script>';
-
 
     }
 
