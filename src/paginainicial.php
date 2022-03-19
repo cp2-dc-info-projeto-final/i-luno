788542001c1f31eb242a91b921f1b4240a5099af
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-18 23:29:21 -0300

    Update paginainicial.php

--- src/paginainicial.php
+++ src/paginainicial.php
@@ -31,6 +31,23 @@
   <a href="sair.php">log out</a>
   </div>
 </div>
+<br><br>
+<div class="dropdown">
+   <a class="btn" href="#">
+   <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-file-plus" viewBox="0 0 16 16">
+  <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+</svg>
+    </a>
+    <div class="dropdown-content">
+  <form enctype="multipart/form-data" action="">
+      <label for=""> Selecione arquivo</label>
+      <input name="arquivo" type="file">
+      <button type="submit"> Enviar arquivo</button>
+  </form>
+  </div>
+    </div>
+
     
 </body>
 
