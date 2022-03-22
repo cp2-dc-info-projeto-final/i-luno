10e6dcebc1e4b1d2c4ed274d2762acfd193f05e5
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-03-22 00:00:35 -0300

    Update conecta.php

--- src/conecta.php
+++ src/conecta.php
@@ -3,7 +3,7 @@
     $dbHost = 'Localhost';
     $dbUsername = 'root';
     $dbPassword = '';
-    $dbName = 'banco';
+    $dbName = 'iluno';
     
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
