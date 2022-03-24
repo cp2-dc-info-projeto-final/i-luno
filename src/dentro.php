e544a93ce9bce64f3f4375e96a95b88ef5291667
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-03-23 22:58:58 -0300

    Update dentro.php

--- src/dentro.php
+++ src/dentro.php
@@ -1,14 +1,13 @@
 <<?php
 
 session_start();
-//por um link pro adm ir direto pra lista,busca,exclusão,edição e atualização
-//usuari normal ir direto pro perfil
-// por opção para o usuario sair 
+ 
 
 if(isset($_SESSION['adm'])){ 
     echo'Bem-vindo'. $_SESSION ['adm'].' - ADMINISTRADOR';
     echo '<script type=" text/javascript">window.location = "admin.php"</script>';
-     }else if (isset($_SESSION['normal'])){  
+     
+  }else if (isset($_SESSION['normal'])){  
         echo 'Bem-vindo'. $_SESSION['normal']. '';
         echo '<script type=" text/javascript">window.location = "paginainicial.php"</script>';
         }else{  
@@ -18,5 +17,3 @@ if(isset($_SESSION['adm'])){
 
 
 ?>
-
-
