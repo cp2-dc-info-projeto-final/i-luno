7affb4661d3cb0cac2154b9fb3d84b63d09596c2
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-24 01:40:44 -0300

    Update testdrive.php

--- src/testdrive.php
+++ src/testdrive.php
@@ -6,7 +6,7 @@ if(isset ($_POST['nomusu']) && isset($_POST['senha'])){
     $nome = $_POST['nomusu'];
     $senha =$_POST['senha'];
 
-    $_GET = mysqli_query("SELECT * FROM usuarios WHERE nome= '$nomusu' AND senha = '$senha' AND  tipo='$tipo'");
+    $_GET = mysqli_query("SELECT * FROM usuarios WHERE nome= '$nomusu' AND senha = '$senha' AND  tipo= '$tipo' ");
     $num = mysqli_num_ruws($_GET);
 
     if ($tipo == 1 ){
