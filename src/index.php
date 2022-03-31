e30da841a5637c0fcfcd8acd58239edef308426c
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-03-31 11:08:39 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,47 @@
+<?php
+include("conecta.php");
+
+if(isset ($_POST['nomUsuario']) || isset($_POST['senha'])){
+  
+    if(strlen($_POST['nomUsuario'])== 0){    
+      echo"Preencha seu nome";
+    }  else if(strlen($_POST['senha'])== 0){  
+      echo"Preencha com sua senha";
+    } else {   
+
+      $nome = $mysli ->real_escape_string($_POST['nomUsuario']);
+      $senha = $mysli ->real_escape_string($_POST['senha']);
+
+
+       $sql_code = "SELECT * FROM  usuarios WHERE nomUsuario = '$nomUsuario' AND senha= '$senha'";
+       $sql_query = $mysqli->query($sql_code) or die ("Falha na execusão do código:" . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1){ 
+            
+            $usuario = $sql_query->mysqli_fetch_assoc();
+
+
+            if(!isset($_SESSION)) {  
+             session_start();
+         }
+           $_SESSION['id'] = $usuario ['id'];
+           $_SESSION['nomUsuario'] = $usuario ['nomUsuario'];
+
+            header("location: paginainicial.php");
+
+        }else{  
+         echo"Falha ao logar!! nome ou senha incorretos";
+        }
+    
+    }   
+
+
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+ <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +55,7 @@
     
     <div class="inputBox1">
         <h1>Login</h1>
-        <form action="testdrive.php" method="POST" name="nomusu">
+        <form action="" method="POST" name="nomusu">
         <input type="text" placeholder="Nome de usuário">
         <br><br>
         <input type="password" placeholder="Senha" name="senha">
@@ -22,6 +64,5 @@
         <br><br><br>
         <a class="btn" href="formulario.php">Cadastra-se aqui</a>
     </div>
-
 </body>
 </html>
