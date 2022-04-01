7f0a976c1cad939a0b3b791c540a69cf451ce9f0
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-31 21:37:21 -0300

    Update admin.php

--- src/admin.php
+++ src/admin.php
@@ -1,15 +1,13 @@
 <?php
     session_start();
     include_once('conecta.php');
-    print_r($_SESSION);
-    if((!isset($_SESSION['nome']) == true) and (!isset($_SESSION['senha']) == true))
+    
+    if((!isset($_SESSION['nomeusu']) == true) and (!isset($_SESSION['senha']) == true))
     {
-        unset($_SESSION['nome']);
+        unset($_SESSION['nomeusu']);
         unset($_SESSION['senha']);
-        
-        
+           
     }
-    $logado = $_SESSION['nome'];
     if(!empty($_GET['search']))
     {
         $data = $_GET['search'];
@@ -19,7 +17,7 @@
     {
         $sql = "SELECT * FROM usuarios ORDER BY id DESC";
     }
-    $result = $conexao->query($sql);
+    $result = $mysqli->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,12 +29,12 @@
     <title>SISTEMA DE ADMINISTRAÇÃO </title>
     <style>
         body{
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+            background-color:#55ACEE;
             color: white;
             text-align: center;
         }
         .table-bg{
-            background: rgba(0, 0, 0, 0.3);
+            background-color: #242424;
             border-radius: 15px 15px 0 0;
         }
 
@@ -48,7 +46,7 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">SISTEMA DE ADMINISTRAÇÃO</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -60,9 +58,6 @@
         </div>
     </nav>
     <br>
-    <?php
-        echo "<h1>Bem vindo <u>$logado</u></h1>";
-    ?>
     <br>
     <div class="box-search">
         <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
@@ -91,6 +86,9 @@
                         echo "<td>".$user_data['senha']."</td>";
                         echo "<td>".$user_data['nomUsuario']."</td>";
                         echo "<td>
+                        
+                        <a class='btn btn-sm btn-danger' href='tornaradmin.php?id=$user_data[id]' title='Deletar'> Tornar admin</a>
+                        
                         <a class='btn btn-sm btn-primary' href='editar.php?id=$user_data[id]' title='EditarUsu'>
                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
                                 <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
@@ -99,8 +97,7 @@
                             <a class='btn btn-sm btn-danger' href='delete.php?id=$user_data[id]' title='Deletar'>
                                 <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
                                     <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
-                                </svg>
-                            </a>
+                                </svg>             
                             </td>";
                         echo "</tr>";
                     }
