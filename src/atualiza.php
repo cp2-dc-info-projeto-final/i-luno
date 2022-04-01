c817ca619e5e309f51ff5df88ebdd3b47686db0b
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-31 23:32:23 -0300

    Update atualiza.php

--- src/atualiza.php
+++ src/atualiza.php
@@ -1,17 +1,19 @@
 <?php
-    include_once('conecta.php');
-
-    if(isset($_POST[ 'atualizar']))
+      
+    include_once ('conecta.php');
+    if (isset($_POST ['atualizar']))
     {
-        $id = $_POST['id'];
+        $id = $_POST ['id'];
         $nome = $_POST['nome'];
-        $nomUsu = $_POST['nomusu'];
+        $nomUsu = $_POST['nomUsu'];
         $senha = $_POST['senha'];
         
-        $sqlUpdate = "UPDATE usuarios SET nome='$nome',senha='$senha',nomusu='$nomusu' WHERE id=$id";
-        $result = $conexao->query($sqlUpdate);
-        print_r($result);
+        
+        $sqlInsert = "ATUALIZAR usuarios
+        SET nome='$nome',senha='$senha',nomUsu='$nomUsu' WHERE id=$id";
+        $resultado = $conexao -> consulta($sqlInsert);
+        print_r($resultado);
     }
-    header('Location: formulario.php');
+     header('Localização: sistema.php');
 
 ?>
