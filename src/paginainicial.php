8bc8f013e70f1393b8121c760f36fe73402e4730
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-31 23:37:26 -0300

    Update paginainicial.php

--- src/paginainicial.php
+++ src/paginainicial.php
@@ -1,7 +1,9 @@
 <?php
-include("seguro.php")
-
+   include("seguro.php");
+   session_start();
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +15,7 @@ include("seguro.php")
     <title>i aluno</title>
     
     <link rel="stylesheet" type="text/css" href="css.css" media="screen" />
+
 </head>
 <body>
     
@@ -22,7 +25,7 @@ include("seguro.php")
         <li class="nav-item">
         <div class="dropdown">
             <a class="btn2" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-gear-wide" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-gear-wide" viewBox="0 0 16 16">
                     <path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"/>
                 </svg>
             </a>
@@ -33,30 +36,36 @@ include("seguro.php")
         </div>
         </li>
       </ul>
+      
       <form class="d-flex">
-        <input class="form-control me-2" type="text" placeholder="Pesquisar">
+        <input class="form-control me-2" type="text" placeholder="Pesquisar" id="pesquisar">
         <button onclick="searchData()" class="btn">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
         </button>
       </form>
+    
     </div>
   </div>
 </nav>
     
 </body>
+
 <script>
     var search = document.getElementById('pesquisar');
+
+    function searchData()
+    {
+        window.location = 'paginainicial.php?search='+search.value;
+    }
+    
     search.addEventListener("keydown", function(event) {
         if (event.key === "Enter") 
         {
             searchData();
         }
     });
-    function searchData()
-    {
-        window.location = 'sistema.php?search='+search.value;
-    }
 </script>
+
 </html>
