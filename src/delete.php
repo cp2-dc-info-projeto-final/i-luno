540173cbae17b23b7f7c29ab151128c7f475e9f7
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-31 21:38:33 -0300

    Update delete.php

--- src/delete.php
+++ src/delete.php
@@ -8,12 +8,12 @@
 
         $sqlSelect = "SELECT * FROM usuarios WHERE id=$id";
 
-        $result = $conexao ->query($sqlSelect);
+        $result = $mysqli ->query($sqlSelect);
 
         if($result->num_rows > 0)
         {
             $sqlDelete = " DELETE FROM usuarios WHERE id=$id";
-            $resultDelete = $conexao ->query($sqlDelete);
+            $resultDelete = $mysqli ->query($sqlDelete);
         }
     }
     header('Location: admin.php');
