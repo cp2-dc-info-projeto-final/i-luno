07328a559f35a6a53de1bd7327bc314cd181f08b
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-03-31 23:29:35 -0300

    Delete interface.php

--- src/interface.php
+++
@@ -1,8 +0,0 @@
-p><strong>Busca de Usuário</strong></p>
-        <form action="busca.php" method="POST">
-            <input type="hidden" name="operacao" value="buscar">
-            <p>pesquisa: <input type="text" name="nomeusu" size="10"> </p>
-            <p><input type="submit" value="Buscar"></p>
-        </form>
-	<body>
-</html>
