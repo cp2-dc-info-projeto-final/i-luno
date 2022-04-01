dd36cd0e556c11dfc2ca1240cd26f02c1b9c4099
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-03-31 21:52:22 -0300

    Create interface.php

---
+++ src/interface.php
@@ -0,0 +1,8 @@
+p><strong>Busca de Usuário</strong></p>
+        <form action="busca.php" method="POST">
+            <input type="hidden" name="operacao" value="buscar">
+            <p>pesquisa: <input type="text" name="nomeusu" size="10"> </p>
+            <p><input type="submit" value="Buscar"></p>
+        </form>
+	<body>
+</html>
